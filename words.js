40779b5ccea0f9c7e8188390a2d8c51d2af03955
Pushpajit Roy <pushpajitroy098@gmail.com>
2025-12-23 00:36:58 +0530

    Delete words.js

--- words.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-    "Apple", "Banana", "Cat", "Dog", "Elephant", "Fish", "Guitar", "House", "Ice Cream", 
-    "Jellyfish", "Kite", "Lion", "Moon", "Nest", "Owl", "Penguin", "Queen", "Robot", 
-    "Sun", "Tree", "Umbrella", "Violin", "Whale", "X-ray", "Yacht", "Zebra", "Computer",
-    "Pizza", "Rocket", "Ghost", "Cactus", "Bicycle", "Camera", "Telescope", "Rainbow"
-];
